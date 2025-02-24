45a633e8186781c49c82e1d2d396897bcb36070a
K LAKSHMI PRASANNA <lakshmiprasannareddy527@gmail.com>
2025-02-24 16:06:52 +0530

    Create Set up a Next.js

---
+++ Set up a Next.js
@@ -0,0 +1,8 @@
+npx create-next-app@latest 
+email-pdf-ingestion --typescript
+cd email-pdf-ingestion
+//Install Dependencies
+npm install prisma @prisma/client 
+postgres imap-simple pdf-parse next-auth
+//Intialize Prisma
+npx prisma init
